930333e5f01423e1f839666923e6f79920c4b830
yuuki2323 <yuuki.work23@gmail.com>
2024-04-03 20:38:48 +0900

    qa

--- src/app/news/[postId]/page.js
+++ src/app/news/[postId]/page.js
@@ -1,4 +1,4 @@
-import parse from 'html-react-parser';
+
 export const runtime = 'edge';
 import React from "react";
 import Header from "../../topComponets/Header.js";
@@ -14,7 +14,7 @@ const Home = async ({ params: { postId } }) => {
       <section className="container mx-auto mt-20 md:mt-32 text-sm md:text-xl text-white">
         <div>
           <h1 className='mb-4 md:mb-12'>{post.title}</h1>
-          <div>{parse(post.content)}</div>
+          <div>{post.content}</div>
         </div>
       </section>
     </main>
