87dd51deea7db0af7cf86257538361966af54ccf
yuuki2323 <yuuki.work23@gmail.com>
2024-04-03 20:31:41 +0900

    e

--- src/app/news/[postId]/page.js
+++ src/app/news/[postId]/page.js
@@ -1,9 +1,8 @@
 import parse from 'html-react-parser';
 export const runtime = 'edge';
 import React from "react";
-import { format } from "date-fns";
 import Header from "../../topComponets/Header.js";
-import { getList,getDetail } from "../../../../libs/client.js";
+import { getDetail } from "../../../../libs/client.js";
 
 
 
@@ -15,8 +14,7 @@ const Home = async ({ params: { postId } }) => {
       <section className="container mx-auto mt-20 md:mt-32 text-sm md:text-xl text-white">
         <div>
           <h1 className='mb-4 md:mb-12'>{post.title}</h1>
-          {/* <h2>{format(new Date(post.createdAt), "yyyy/MM/dd")}</h2> */}
-          <div>{parse(post.contant)}</div>
+          <div>{parse(post.content)}</div>
         </div>
       </section>
     </main>
