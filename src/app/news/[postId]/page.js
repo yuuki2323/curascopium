ae8b5fd7396ab4b3ac2b5c4dcf32ae232ba7f7a5
yuuki2323 <yuuki.work23@gmail.com>
2024-04-04 15:56:05 +0900

    r

--- src/app/news/[postId]/page.js
+++ src/app/news/[postId]/page.js
@@ -8,13 +8,14 @@ import { getDetail } from "../../../../libs/client.js";
 
 const Home = async ({ params: { postId } }) => {
   const post = await getDetail(postId);
+const html = post.content
   return (
     <main className="mx-8">
       <Header />
       <section className="container md:mx-auto mt-24 md:mt-36 text-sm md:text-xl text-white">
         <div>
           <h1 className='mb-4 md:mb-12'>{post.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: htmlString }}>{post.content}</div>
+          <div dangerouslySetInnerHTML={{ __html: html }}></div>
         </div>
       </section>
     </main>
