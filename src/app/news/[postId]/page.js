55df4636dfbcaef24936a6aeef3cc604e42e87ec
yuuki2323 <yuuki.work23@gmail.com>
2024-04-09 11:19:11 +0900

    e

--- src/app/news/[postId]/page.js
+++ src/app/news/[postId]/page.js
@@ -3,7 +3,7 @@ export const runtime = 'edge';
 import React from "react";
 import Header from "../../topComponets/Header.js";
 import { getDetail } from "../../../../libs/client.js";
-
+import { format } from "date-fns";
 
 
 const Home = async ({ params: { postId } }) => {
@@ -12,10 +12,11 @@ const html = post.content
   return (
     <main className="mx-8">
       <Header />
-      <section className="container md:mx-auto mt-24 md:mt-36 text-sm md:text-xl text-white">
+      <section className="container mx-auto mt-24 md:mt-36 text-sm md:text-lg text-white">
         <div>
-          <h1 className='mb-4 md:mb-12 text-lg md:text-2xl '>{post.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: html }}></div>
+          <p className="text-center mb-2 md:mb-6">{format(new Date(post.createdAt), "yyyy/MM/dd")}</p>
+          <h2 className='mb-4 md:mb-12 text-2xl md:text-4xl text-center'>{post.title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: html }} className="mx-8"></div>
         </div>
       </section>
     </main>
