2958064b91201d9e0290816a938cbc80c42c2edc
Gaku Yamada <54490890+shinzan85@users.noreply.github.com>
2024-04-11 15:54:00 -0700

    Update page.js

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -14,7 +14,7 @@ const Home = async () => {
         <Title en="NEWS" ja="お知らせ" />
         {contents.map((post) => (
           <dl
-            className="text-white border-b border-white flex gap-2 md:gap-8 py-2 mb-2"
+            className="text-white border-b border-white flex gap-2 md:gap-8 py-2 mb-2 tabular-nums"
             key={post.id}
           >
             <dt>{format(new Date(post.createdAt), "yyyy/MM/dd")}</dt>
