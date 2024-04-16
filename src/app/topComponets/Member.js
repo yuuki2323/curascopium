5932c8708201c0c4cb958e3e8808df8c5fed9f64
tora_0210 <toratta0210@gmail.com>
2024-04-15 17:46:08 -0700

    responsive membercard

--- src/app/topComponets/Member.js
+++ src/app/topComponets/Member.js
@@ -17,16 +17,16 @@ const MemberCard = ({ id, name, description }) => {
   });
   return (
     <SplideSlide>
-      <div className='w-[370] h-[480]  bg-blue-950 bg-bg bg-contain p-8 text-center text-white'>
+      <div className='w-[370] h-[480]  bg-blue-950 bg-bg bg-contain p-3 md:p-8 text-center text-white'>
         <Image
           src={`/profile/${id}.png`}
           alt='topimage'
           width={300}
           height={300}
-          className='w-screen h-auto hidden md:block rounded-full mb-2 p-4'
+          className='w-screen h-auto rounded-full mb-2 md:p-4'
         />
-        <p className='text-2xl mb-4'>{name}</p>
-        <p className='text-base h-16'>{formattedText}</p>
+        <p className='text-sm md:text-2xl mb-2 md:mb-4'>{name}</p>
+        <p className='text-[8px] md:text-base h-10 md:h-16'>{formattedText}</p>
       </div>
     </SplideSlide>
   );
@@ -46,91 +46,29 @@ const Member = () => {
         />
       </div>
 
-      <div className='hidden md:block'>
-        <Splide
-          options={{
-            autoplay: true,
-            interval: 4000,
-            type: 'loop',
-            speed: 1200,
-            perPage: 3,
-            perMove: 1,
-            gap: 24,
-            pagination: false,
-          }}
-          aria-label='profile-photo'>
-          {members.map((member) => {
-            return (
-              <MemberCard
-                key={member[0]}
-                id={member[0]}
-                name={member[1].name}
-                description={member[1].description}
-              />
-            );
-          })}
-        </Splide>
-      </div>
-      <div className='md:hidden block'>
-        <Splide
-          options={{
-            autoplay: true,
-            interval: 4000,
-            type: 'loop',
-            speed: 1200,
-            perPage: 2,
-            perMove: 1,
-            gap: 24,
-            pagination: false,
-          }}
-          aria-label='profile-photo'>
-          <SplideSlide>
-            <div className='w-[370] h-[480] bg-blue-950 bg-bg bg-contain p-3 text-center text-white'>
-              <Image
-                src='/profile/tanaka.png'
-                alt='topimage'
-                width={160}
-                height={160}
-                className=' w-screen h-auto md:hidden block rounded-full mb-2'
-              />
-              <p className='text-sm mb-2 '>田中 優作</p>
-              <p className='text-[8px] h-10'>
-                カリフォルニア大学バークレー校学部4年生。宇宙物理学専攻、障害者学副専攻。
-              </p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='w-[370] h-[480] bg-blue-950 bg-bg bg-contain p-3 text-center text-white'>
-              <Image
-                src='/profile/yamamoto.png'
-                alt='topimage'
-                width={160}
-                height={160}
-                className=' w-screen h-auto md:hidden block rounded-full mb-2'
-              />
-              <p className='text-sm mb-2 '>山本 暖</p>
-              <p className='text-[8px] h-10'>
-                カリフォルニア大学バークレー校卒。生物学専攻、データサイエンス副専攻
-              </p>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className='w-[370] h-[480] bg-blue-950 bg-bg bg-contain p-3 text-center text-white'>
-              <Image
-                src='/profile/nagasawa.png'
-                alt='topimage'
-                width={160}
-                height={160}
-                className=' w-screen h-auto md:hidden block rounded-full mb-2'
-              />
-              <p className='text-sm mb-2 '>長澤 赳志</p>
-              <p className='text-[8px] h-10'>
-                カリフォルニア大学バークレー校卒。宇宙物理学専攻。
-              </p>
-            </div>
-          </SplideSlide>
-        </Splide>
-      </div>
+      <Splide
+        options={{
+          autoplay: true,
+          interval: 4000,
+          type: 'loop',
+          speed: 1200,
+          perPage: 3,
+          perMove: 1,
+          gap: 24,
+          pagination: false,
+        }}
+        aria-label='profile-photo'>
+        {members.map((member) => {
+          return (
+            <MemberCard
+              key={member[0]}
+              id={member[0]}
+              name={member[1].name}
+              description={member[1].description}
+            />
+          );
+        })}
+      </Splide>
     </section>
   );
 };
