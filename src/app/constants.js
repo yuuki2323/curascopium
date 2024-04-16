2b0b79528a264d7061608d5d22609c7ae9a0d654
tora_0210 <toratta0210@gmail.com>
2024-04-15 18:07:42 -0700

    update members

--- src/app/constants.js
+++ src/app/constants.js
@@ -11,10 +11,6 @@ export const SNS = Object.freeze({
     href: 'https://www.linkedin.com/company/curascopium138/',
     src: '/sns/linkedIn.png',
   },
-  github: {
-    href: 'https://www.linkedin.com/company/curascopium138/',
-    src: '/sns/linkedIn.png',
-  },
 });
 
 export const Navi = Object.freeze({
@@ -42,17 +38,13 @@ export const Navi = Object.freeze({
     href: '/#contact',
     nav: 'お問い合わせ',
   },
-  contacts: {
-    href: '/#contact',
-    nav: 'お問い合わせせせ',
-  },
 });
 
 export const MemberProfile = Object.freeze({
   tanaka: {
     name: '田中 優作',
     description:
-      'カリフォルニア大学バークレー校学部4年\n宇宙物理学専攻、障害者学副専攻',
+      'カリフォルニア大学バークレー校学部\n宇宙物理学専攻、障害者学副専攻',
   },
   yamamoto: {
     name: '山本 暖',
@@ -63,14 +55,26 @@ export const MemberProfile = Object.freeze({
     name: '長澤 赳志',
     description: 'カリフォルニア大学バークレー校卒\n宇宙物理学専攻',
   },
+  sanada: {
+    name: '眞田 聖光',
+    description: '東京大学卒業[惑星科学]\nテキサス大学院[惑星科学]',
+  },
+  harimoto: {
+    name: '張本 宏光',
+    description:
+      'カリフォルニア大学バークレー校卒 [Biochemistry/Data Science]\nマサチューセッツ工科大学 元研究員\n東京大学医科学研究所 研究員',
+  },
+  miyagi: {
+    name: '宮城 理',
+    description: '早稲田大学卒業 [国際教養学部]',
+  },
   taniguchi: {
     name: '谷口 知佳',
     description:
-      'カリフォルニア大学バークレー校修士1年\nランドスケープアーキテクチャ専攻',
+      'カリフォルニア大学バークレー校修士\nランドスケープアーキテクチャ専攻',
   },
   yamada: {
     name: '山田 楽',
-    description:
-      'カリフォルニア大学バークレー校4年\nコンピューターサイエンス専攻',
+    description: 'カリフォルニア大学バークレー校\nコンピューターサイエンス専攻',
   },
 });
