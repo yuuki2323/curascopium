fb8d0db6b939c097c8f23a4d7815da28dbba95db
tora_0210 <toratta0210@gmail.com>
2024-04-26 13:15:11 -0700

    Update messages and Topphoto layout

--- src/app/topComponets/Topphoto.js
+++ src/app/topComponets/Topphoto.js
@@ -14,7 +14,7 @@ const Topphoto = () => {
         height={1280}
         className=" object-cover md:hidden block w-screen h-screen"
       />
-      <p className=" text-white text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  w-5/6 md:hidden text-3xl">
+      <p className=" text-white text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:hidden text-3xl">
         {t.rich("Mobile", { br: () => <br /> })}
       </p>
       {/* PC用 */}
