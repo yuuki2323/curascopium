8f93391a52d81545f5cf41b0af138597606f1c9a
tora_0210 <toratta0210@gmail.com>
2024-04-26 03:44:42 -0700

    i18n for contact section

--- src/app/topComponets/Contact.js
+++ src/app/topComponets/Contact.js
@@ -1,101 +1,95 @@
-import React from 'react';
-import Title from '../ui/Title';
-import CustomSection from '../ui/CustomSection';
+import React from "react";
+import CustomSection from "../ui/CustomSection";
+import { useTranslations } from "next-intl";
 
 const Contact = () => {
+  const t = useTranslations("contact");
   return (
     <CustomSection
-      id='contact'
-      body={<ContactBody />}
+      id="contact"
+      title={t("title")}
+      body={<ContactBody t={t} />}
     />
   );
 };
 
-const ContactBody = () => {
+const ContactBody = ({ t }) => {
   return (
-    <div className='text-white text-sm md:text-lg'>
+    <div className="text-white text-sm md:text-lg">
       <form
-        action='https://ssl.form-mailer.jp/fm/service/Forms/confirm'
-        method='post'
-        name='form1'
-        encType='multipart/form-data'
-        acceptCharset='UTF-8'
-        className='bg-contact mx-auto md:w-3/4 bg-bg bg-contain py-5'>
-        <input
-          type='hidden'
-          name='key'
-          value='d7efceec813650'
-        />
-        <div className='w-10/12 md:w-8/12 mx-auto'>
-          <div className='mb-4'>
-            <p>氏名</p>
+        action="https://ssl.form-mailer.jp/fm/service/Forms/confirm"
+        method="post"
+        name="form1"
+        encType="multipart/form-data"
+        acceptCharset="UTF-8"
+        className="bg-contact mx-auto md:w-3/4 bg-bg bg-contain py-5">
+        <input type="hidden" name="key" value="d7efceec813650" />
+        <div className="w-10/12 md:w-8/12 mx-auto">
+          <div className="mb-4">
+            <p>{t("form.name.title")}</p>
             <p>
               <input
-                name='field_7105057'
-                type='text'
-                size='30'
-                placeholder='例)山田 太郎'
-                className=' w-full p-1 text-black'
+                name="field_7105057"
+                type="text"
+                size="30"
+                placeholder={t("form.name.placeholder")}
+                className=" w-full p-1 text-black"
               />
             </p>
           </div>
-          <div className='mb-4'>
-            <p>連絡先</p>
+          <div className="mb-4">
+            <p>{t("form.phone.title")}</p>
             <p>
               <input
-                name='field_7105050'
-                type='text'
-                placeholder='例)06-0000-0000'
-                className=' w-full p-1 text-black'
+                name="field_7105050"
+                type="text"
+                placeholder={t("form.phone.placeholder")}
+                className=" w-full p-1 text-black"
               />
             </p>
           </div>
-          <div className='mb-4'>
-            <p>メールアドレス</p>
+          <div className="mb-4">
+            <p>{t("form.email.title")}</p>
             <p>
               <input
-                name='field_7105015'
-                type='text'
-                size='30'
-                placeholder='例)sample@sample.com'
-                className=' w-full p-1 text-black'
+                name="field_7105015"
+                type="text"
+                size="30"
+                placeholder={t("form.email.placeholder")}
+                className=" w-full p-1 text-black"
               />
             </p>
           </div>
-          <div className='mb-4'>
-            <p>お問い合わせ内容</p>
+          <div className="mb-4">
+            <p>{t("form.message.title")}</p>
             <p>
               <textarea
-                name='field_7105025'
-                cols='30'
-                rows='8'
-                placeholder=''
-                className='w-full p-1 text-black'></textarea>
+                name="field_7105025"
+                cols="30"
+                rows="8"
+                placeholder=""
+                className="w-full p-1 text-black"></textarea>
             </p>
           </div>
-          <p className='text-center'>
+          <p className="text-center">
             <input
-              name='submit'
-              type='submit'
-              value='確認画面へ'
-              className='bg-title text-black py-2 px-12 rounded-sm cursor-pointer shadow-lg'
+              name="submit"
+              type="submit"
+              value={t("form.submit")}
+              className="bg-title text-black py-2 px-12 rounded-sm cursor-pointer shadow-lg"
             />
           </p>
-          <p className='text-xs text-white/50 text-right pt-5'>
+          <p className="text-xs text-white/50 text-right pt-5">
             <a
-              href='https://www.form-mailer.jp/?utm_source=https://ssl.form-mailer.jp/fms/d7efceec813650&utm_medium=ownedmedia&utm_campaign=powered-by-formmailer'
-              target='_blank'
-              id='powered'>
+              href="https://www.form-mailer.jp/?utm_source=https://ssl.form-mailer.jp/fms/d7efceec813650&utm_medium=ownedmedia&utm_campaign=powered-by-formmailer"
+              target="_blank"
+              id="powered">
               Powered by FormMailer.
             </a>
           </p>
         </div>
 
-        <input
-          type='hidden'
-          name='code'
-          value='utf8'
-        />
+        <input type="hidden" name="code" value="utf8" />
       </form>
     </div>
   );
