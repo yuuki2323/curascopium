522608a941ddc50f4eeb0336874f769868cb2de5
tora_0210 <toratta0210@gmail.com>
2024-04-26 00:27:51 -0700

    next-intl setup

---
+++ src/app/[locale]/page.js
@@ -0,0 +1,28 @@
+import Header from "../topComponets/Header";
+import Topphoto from "../topComponets/Topphoto";
+import News from "../topComponets/News";
+import History from "../topComponets/History";
+import Purpose from "../topComponets/Purpose";
+import Member from "../topComponets/member/Member";
+import Project from "../topComponets/Project";
+import Contact from "../topComponets/Contact";
+import ReturnTopButton from "../topComponets/ReturnTopButton";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Topphoto />
+
+      <div className="mx-8 md:mx-0">
+        <News />
+        <History />
+        <Purpose />
+        <Member />
+        <Project />
+        <Contact />
+        <ReturnTopButton />
+      </div>
+    </main>
+  );
+}
