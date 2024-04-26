8e6df95bfcc88115e56d591d6b34f1a38bf9802a
tora_0210 <toratta0210@gmail.com>
2024-04-26 12:40:40 -0700

    i18n member

--- src/app/constants.js
+++ src/app/constants.js
@@ -40,71 +40,19 @@ export const SectionInfo = Object.freeze({
   },
 });
 
-export const MemberProfile = Object.freeze({
-  tanaka: {
-    name: "田中 優作",
-    alphabet: "Yusaku Tanaka",
-    description: "UC バークレー\n[宇宙物理学 / 障がい者学]",
-  },
-  yamamoto: {
-    name: "山本 暖",
-    alphabet: "Haru Yamamoto",
-    description:
-      "UC バークレー卒\n[分子細胞生物学 / データサイエンス] \n イェール大学 医学部\n[MD-PhD]",
-  },
-  nagasawa: {
-    name: "長澤 赳志",
-    alphabet: "Takeshi Nagasawa",
-    description: "UC バークレー卒\n[宇宙物理学]",
-  },
-  sanada: {
-    name: "眞田 聖光",
-    alphabet: "Saniya Sanada",
-    description:
-      "東京大学 大学院卒\n[惑星科学]\nUT サンアントニオ 大学院\n[惑星科学]",
-  },
-  harimoto: {
-    name: "張本 宏光",
-    alphabet: "Hiromitsu Harimoto",
-    description:
-      "UC バークレー卒\n[生化学 / データサイエンス]\nMIT 元研究員\n東京大学医科学研究所 研究員",
-  },
-  miyagi: {
-    name: "宮城 理",
-    alphabet: "Satoshi Miyagi",
-    description: "早稲田大学卒\n[国際教養学]",
-  },
-  taniguchi: {
-    name: "谷口 知佳",
-    alphabet: "Tomoka Taniguchi",
-    description:
-      "上智大学卒\n[政治学 / 社会科学]\nUC バークレー修士\n[ランドスケープアーキテクチャ]",
-  },
-  yamada: {
-    name: "山田 楽",
-    alphabet: "Gaku Yamada",
-    description: "UC バークレー\n[コンピューターサイエンス]",
-  },
-  kurosaka: {
-    name: "黒坂 美桜",
-    alphabet: "Mio Kurosaka",
-    description: "UC バークレー\n[芸術実践]",
-  },
-  sato: {
-    name: "佐藤 さくや",
-    alphabet: "Sakuya Sato",
-    description: "UC バークレー\n[社会環境学 / コンピューティングミュージック]",
-  },
-  matsunaga: {
-    name: "松永 大志",
-    alphabet: "Taishi Matsunaga",
-    description: "UC バークレー\n[政治経済学 / データサイエンス]",
-  },
-  ikeda: {
-    name: "池田 夢叶",
-    alphabet: "Yumeka Ikeda",
-    description: "オレンジコーストカレッジ\n[宇宙物理学]",
-  },
-});
+export const members = [
+  "tanaka",
+  "yamamoto",
+  "nagasawa",
+  "sanada",
+  "harimoto",
+  "miyagi",
+  "taniguchi",
+  "yamada",
+  "kurosaka",
+  "sato",
+  "matsunaga",
+  "ikeda",
+];
 
 export const projects = ["education", "planetarium"];
--- src/app/topComponets/member/Member.js
+++ src/app/topComponets/member/Member.js
@@ -1,11 +1,27 @@
 import React from "react";
+import pick from "lodash/pick";
 import CustomSection from "../../ui/CustomSection";
-import { useTranslations } from "next-intl";
+import {
+  NextIntlClientProvider,
+  useMessages,
+  useTranslations,
+} from "next-intl";
 import { MemberBody } from "./MemberBody";
 
 const Member = () => {
   const t = useTranslations("member");
-  return <CustomSection id="member" title={t("title")} body={<MemberBody />} />;
+  const msg = useMessages();
+  return (
+    <CustomSection
+      id="member"
+      title={t("title")}
+      body={
+        <NextIntlClientProvider messages={pick(msg, "member")}>
+          <MemberBody />
+        </NextIntlClientProvider>
+      }
+    />
+  );
 };
 
 export default Member;
