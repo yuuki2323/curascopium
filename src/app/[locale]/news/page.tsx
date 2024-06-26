9683995bb7eec90430307c4579ca3dbc0b067b74
shinzan85 <toratta0210@gmail.com>
2024-06-26 02:20:20 -0700

    refactor: Update News page to use News component for displaying news

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -4,33 +4,17 @@ import Title from "../../ui/Title";
 import Header from "../../topComponets/Header";
 import { Link } from "@/navigation";
 import { MicroCMSPost, getList } from "../../../../libs/client";
+import News from "@/app/topComponets/News";
 
 export const runtime = "edge";
 
 const Home = () => {
   return (
-    <main className="mx-8">
+    <>
       <Header />
-      <section className="container mx-auto mt-20 md:mt-32 text-sm md:text-xl">
-        <Title en="NEWS" ja="お知らせ" />
-        <NewsBody />
-      </section>
-    </main>
+      <News />
+    </>
   );
-};
-
-const NewsBody = async () => {
-  const { contents } = await getList({ limit: 5 });
-  return contents.map((post: MicroCMSPost) => (
-    <dl
-      className="text-white border-b border-white flex gap-2 md:gap-8 py-2 mb-2 tabular-nums"
-      key={post.id}>
-      <dt>{format(new Date(post.createdAt), "yyyy/MM/dd")}</dt>
-      <Link href={`/news/${post.id}`}>
-        <dd>{post.title}</dd>
-      </Link>
-    </dl>
-  ));
 };
 
 export default Home;
